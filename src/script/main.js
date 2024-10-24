dff42d6f0d8e47189e48a4e8ed7a2105daecf4f6
JoaoACastro <joaoaccastro@gmail.com>
2024-10-24 17:13:00 -0300

    hidden inputs, colors and button generate. Show other button to reset page. Includ option to save qrcode

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,7 @@
 function generateQRCode() {
   const bgColor = document.getElementById("bg-color").value;
   const qrcodeColor = document.getElementById("qrcode-color").value;
-  const qrcodeSize =  document.getElementById("qrcode-size").value;
+  const qrcodeSize = document.getElementById("qrcode-size").value;
 
   document.getElementById("qrcode").innerHTML = ""; // Limpar o QR Code anterior
 
@@ -20,4 +20,40 @@ function generateQRCode() {
     colorLight: bgColor,
     correctLevel: QRCode.CorrectLevel.H, // Níivel de correção de erro
   });
+
+  hiddenLabelAndColor();
+  changeButton();
 }
+
+const state = {
+  container: {
+    label: document.getElementById("label"),
+    color: document.getElementById("qrCodeColor"),
+    generateButton: document.getElementById("generateQRCode"),
+    resetButton: document.getElementById("resetButton"),
+  },
+};
+
+function hiddenLabelAndColor() {
+  state.container.label.style.display = "none";
+  state.container.color.style.display = "none";
+}
+
+function changeButton() {
+  state.container.generateButton.style.display = "none";
+  state.container.resetButton.style.display = "flex";
+}
+
+const qrcode = document.getElementById("qrcode");
+
+document.getElementById("saveQrCode").addEventListener("click", function () {
+  const qrCanvas = document.querySelector("#qrcode canvas");
+
+  if (qrCanvas) {
+    const link = document.createElement("a");
+    link.href = qrCanvas.toDataURL();
+    link.download = "qrcode.png";
+
+    link.click();
+  }
+});
